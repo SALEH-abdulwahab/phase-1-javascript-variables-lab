e5a46de3d2ed860b46acc8b49672b46532408454
SALEH-abdulwahab <saleh200228@gmail.com>
2025-06-15 15:44:14 +0300

    complete lap

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+let companyName = "scuber"
+const companyName = "scuber";
+let mostProfitableNeighborhoo;
+ mostProfitableNeighborhoo = "chelsea";
+let companyCeo;
+companyCeo = "Susan Smith"
